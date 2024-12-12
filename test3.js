d71d4286de997694d3e227177204d0a4d5ba8b4e
matchaism <60777705+matchaism@users.noreply.github.com>
2024-12-12 16:59:58 +0900

    add test3.js

---
+++ test3.js
@@ -0,0 +1,63 @@
+void((function(f){
+    var script = document.createElement('script');
+    script.src = '//code.jquery.com/ui/1.13.2/jquery-ui.min.js';
+    script.onload = function(){
+      var $ = jQuery.noConflict(true);
+      f($);
+    };
+    document.body.appendChild(script);
+  }
+)(
+  function($, undefined){
+    let element = document.getElementById('datepicker');
+    element.insertAdjacentHTML('beforebegin', '<div style="top:0px;" class="date-picker-div" id="datepicker_rev"></div>');
+    element.insertAdjacentHTML('beforebegin', '<div id="time_list"></div>');
+
+    let div = document.getElementById('time_list');
+    div.insertAdjacentHTML('beforeend', '<input style="top:280px;" class="date-picker-div" type="text">');
+    div.insertAdjacentHTML('beforeend', '<input style="top:300px;" class="date-picker-div" type="text">');
+    div.insertAdjacentHTML('beforeend', '<input style="top:320px;" class="date-picker-div" type="text">');
+    div.insertAdjacentHTML('beforeend', '<input style="top:340px;" class="date-picker-div" type="text">');
+    div.insertAdjacentHTML('beforeend', '<input style="top:360px;" class="date-picker-div" type="text">');
+    element.remove();
+
+    $(function() {
+      $("#datepicker_rev").datepicker({
+        dateFormat: 'm/d(DD)',
+        firstDay: 0,
+        yearSuffix: '年',
+        showMonthAfterYear: true,
+        monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        dayNames: ['日', '月', '火', '水', '木', '金', '土'],
+        dayNamesMin: ['日', '月', '火', '水', '木', '金', '土'],
+        minDate: new Date(),
+        maxDate: '+12m',
+        hideIfNoPrevNext: true,
+        onSelect: function (dateText, inst) {
+          let message;
+          let time_list = [];
+          let time = document.getElementById("time_list");
+          for(let i = 0; i<time.childElementCount; i++){
+            if(time.children[i].value === "") continue;
+            time_list.push(time.children[i].value);
+          }
+          if(time_list.length === 0) {
+            message = dateText + "\n";
+          } else {
+            message = "";
+            for(let i = 0; i<time_list.length; i++){
+              message += dateText + " " + time_list[i] + "～\n";
+            }
+          }
+          let nowText = $('#kouho').val();
+          if (nowText === "") {
+            $("#kouho").val(message);
+          }
+          else {
+            $("#kouho").val(nowText + message);
+          }
+        }
+      });
+    });
+  }
+));
